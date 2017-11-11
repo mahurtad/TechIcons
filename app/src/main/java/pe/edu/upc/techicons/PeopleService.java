165b94ef54d8c610bc05090db8fcb8633db53fcb
mahurtad <nicamis@gmail.com>
2017-11-11 10:13:35 -0500

    MyCustomers2

--- app/src/main/java/pe/edu/upc/techicons/PeopleService.java
+++ app/src/main/java/pe/edu/upc/techicons/PeopleService.java
@@ -20,21 +20,25 @@ public class PeopleService {
                 res.getString(R.string.title_former_ceo_and_founder),
                 res.getString(R.string.company_microsoft),
                 res.getString(R.string.bio_bill_gates),
+                res.getString(R.string.service_microsoft),
                 R.drawable.manuel));
         people.add(new Person(res.getString(R.string.name_larry_ellison),
                 res.getString(R.string.title_cto),
                 res.getString(R.string.company_oracle),
                 res.getString(R.string.bio_larry_ellison),
+                res.getString(R.string.service_oracle),
                 R.drawable.marco));
         people.add(new Person(res.getString(R.string.name_mark_zuckerberg),
                 res.getString(R.string.title_ceo),
                 res.getString(R.string.company_facebook),
                 res.getString(R.string.bio_mark_zuckerberg),
+                res.getString(R.string.service_facebook),
                 R.drawable.samuel));
         people.add(new Person(res.getString(R.string.name_steve_jobs),
                 res.getString(R.string.title_former_ceo_and_founder),
                 res.getString(R.string.company_apple),
                 res.getString(R.string.bio_steve_jobs),
+                res.getString(R.string.service_apple),
                 R.drawable.saul));
            }
 }
--- app/src/main/java/pe/edu/upc/techicons/Person.java
+++ app/src/main/java/pe/edu/upc/techicons/Person.java
@@ -9,12 +9,14 @@ public class Person {
     private String title;
     private String company;
     private String bio;
+    private String service;
     private int pictureId;
-    public Person(String name, String title, String company, String bio, int pictureId)
+    public Person(String name, String title, String company, String bio, String service, int pictureId)
     {
         this.bio = bio;
         this.company = company;
         this.name = name;
+        this.service = service;
         this.pictureId = pictureId;
         this.title = title;
     }
@@ -26,6 +28,10 @@ public class Person {
     public void setCompany(String company) { this.company = company; }
     public String getBio() { return bio; }
     public void setBio(String bio) { this.bio = bio; }
+
+    public String getService() { return service; }
+    public void setService(String service) { this.service = service; }
+
     public int getPictureId() { return pictureId; }
     public void setPictureId(int pictureId) { this.pictureId = pictureId; }
 }
